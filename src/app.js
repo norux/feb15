0482c390203098b77443446b4c18f753ea7b3534
Andrew Heebum Kwak <andrew@norux.me>
2019-09-03 00:22:09 +0900

    Initialize css setting (#1)

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
+import Container from '@material-ui/core/Container'
 import Button from '@material-ui/core/Button'
 
 export const App = hot(() => (
   <div>
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <Container maxWidth="sm" className="wrap">
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </Container>
   </div>
 ))
