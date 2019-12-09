4761ef72c82c6e1857fab421b2b3b6534ab8ed9d
norux <andrew@norux.me>
2019-12-10 00:44:00 +0900

    Fix bug to target container error

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom'
 import { App } from '~/App'
 import '~/css/index.css'
 
-ReactDOM.render(<App />, document.querySelector('#app'))
+ReactDOM.render(<App />, document.getElementById('app'))
