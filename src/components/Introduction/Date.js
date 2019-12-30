bf6d599181d102f3c30a35cee57d7f2abe6e65a6
norux <andrew@norux.me>
2019-12-30 14:34:21 +0900

    Add to calendar

--- src/components/Introduction/Date.js
+++ src/components/Introduction/Date.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import makeStyles from '@material-ui/core/styles/makeStyles'
 import { Typography } from '@material-ui/core'
 import Box from '@material-ui/core/Box'
+import Link from '@material-ui/core/Link'
+import { addToCalendar } from '~/utils'
+import InsertInvitationIcon from '@material-ui/icons/InsertInvitation';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -15,11 +18,30 @@ const useStyles = makeStyles(() => ({
     marginTop: '2pt',
     fontWeight: '600',
   },
+  calendarLink: {
+    marginTop: '10px',
+    fontSize: '1.3rem',
+
+    '& svg': {
+      verticalAlign: 'bottom',
+      marginRight: '2px',
+    }
+  },
 }))
 
 export const Date = ({ count }) => {
-  const { root, date, location } = useStyles()
-
+  const { root, date, location, calendarLink } = useStyles()
+  const calendarContent = useMemo(
+    () =>
+      addToCalendar(
+        '곽희범 ♥ 용희원 결혼식',
+        '신랑: 곽윤호/한용래의 장남 희범\\n신부: 용경환/김은희의 장녀 희원\\n\\n결혼식에 초대합니다.',
+        '네이버 그린팩토리 2층 커넥트홀',
+        '2020-02-15T13:00:00',
+        '2020-02-15T15:00:00',
+      ),
+    [],
+  )
   return (
     <Box className={root}>
       <Typography variant="body1" className={date} align="center">
@@ -28,6 +50,12 @@ export const Date = ({ count }) => {
       <Typography variant="body1" className={location} align="center">
         <span className="naver-color">네이버</span> 그린팩토리
       </Typography>
+      <Link href={calendarContent} download="feb15" align="center" color="inherit">
+        <Typography variant="body2" className={calendarLink} align="center">
+          <InsertInvitationIcon />
+          캘린더에 등록하기
+        </Typography>
+      </Link>
     </Box>
   )
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,57 @@
+import uuid from 'uuid/v1'
+
 const images = require.context('~/css/images', true)
+
 export function getImage(name) {
   return images(`./${name}`)
 }
 
 export function isWideScreen() {
   return window.innerWidth >= 600
+}
+
+export function isDate(date) {
+  return date instanceof Date
+}
+
+export function toCalendarString(date) {
+  const isoString = date.toISOString()
+  return isoString
+    .replace(/[-:]/g, '')
+    .replace(/\.[0-9]{3}/g, '')
+}
+
+export function addToCalendar(subject, description, location, begin, end) {
+  const SEPARATOR = '\n'
+  const MIME_TYPE = 'data:text/calendar'
+  const CHARSET = 'charset=utf8'
+
+  const BEGIN_CALENDAR = ['BEGIN:VCALENDAR', 'PRODID:Calendar', 'VERSION:2.0']
+  const END_CALENDAR = ['END:VCALENDAR']
+
+  const beginDate = isDate(begin) ? begin : new Date(begin)
+  const endDate = isDate(end) ? end : new Date(end)
+
+  console.log(beginDate, endDate)
+
+  const events = [
+    'BEGIN:VEVENT',
+    `UID:${uuid()}`,
+    'CLASS:PUBLIC',
+    `DTSTAMP:${toCalendarString(new Date())}`,
+    `DTSTART:${toCalendarString(beginDate)}`,
+    `DTEND:${toCalendarString(endDate)}`,
+    `SUMMARY: ${subject}`,
+    `DESCRIPTION:${description}`,
+    `LOCATION:${location}`,
+    'TRANSP:TRANSPARENT',
+    'BEGIN:VALARM',
+    'TRIGGER:-P2D',
+    'ACTION:DISPLAY',
+    'END:VALARM',
+    'END:VEVENT',
+  ]
+
+  const icsFormatString = Array.prototype.concat(BEGIN_CALENDAR, events, END_CALENDAR).join(SEPARATOR)
+  return URL.createObjectURL(new Blob([icsFormatString], { type: 'text/calendar' }))
 }
