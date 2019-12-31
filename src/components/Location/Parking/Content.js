0113d2d2781ede9d4efdb7a31bcbd52052e885ef
norux <andrew@norux.me>
2019-12-31 16:25:59 +0900

    Add content for parking

--- src/components/Location/Parking/Content.js
+++ src/components/Location/Parking/Content.js
@@ -7,14 +7,40 @@ const useStyles = makeStyles(() => ({
   root: {
     marginTop: '20px',
   },
+  section: {
+    '&:not(:nth-child(1))': {
+      marginTop: '12px',
+    },
+  },
 }))
 
 export const Content = () => {
-  const { root } = useStyles()
+  const { root, section } = useStyles()
   return (
     <Box className={root}>
-      <Typography variant="body2">네이버 사옥 주차장 1일 주차권 지급</Typography>
-      <Typography variant="body2">주차권 발급: 지하 1층 식권접수 도우미</Typography>
+      <dl className={section}>
+        <dd>
+          <Typography variant="body2">네이버 사옥의 주차장을 이용하실 수 있습니다.</Typography>
+        </dd>
+      </dl>
+      <dl className={section}>
+        <dt>
+          <Typography variant="subtitle2">주차권 발급</Typography>
+        </dt>
+        <dd>
+          <Typography variant="body2">1일 주차권을 제공합니다.</Typography>
+        </dd>
+        <dd>
+          <Typography variant="body2">지하 1층 식권 접수 도우미에게 요청 해 주세요.</Typography>
+        </dd>
+      </dl>
+      <dl className={section}>
+        <dd>
+          <Typography variant="body2">
+            * 주차권을 발급받지 않고 퇴장 시, 주차요금이 부과됩니다. 반드시 식권 접수 도우미에게 요청 해 주세요.
+          </Typography>
+        </dd>
+      </dl>
     </Box>
   )
 }
